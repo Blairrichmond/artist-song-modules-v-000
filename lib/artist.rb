53409dc1c5978888943892a3b18125044e76fc19
Brent  Ransom <sadumedusa@gmail.com>
2018-02-21 21:30:32 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-
+  extend Findable
+  include Memorable::InstanceMethods
+  include Paramable
   attr_accessor :name
   attr_reader :songs
 
